1309b488b31bee35cb9883247e1c19ce1d9f269b
Barry <barry.tolnas@providence.org>
2017-08-10 23:29:11 -0700

    remove references to deleted files

---
+++ Osiris/AvailabilityView.swift
@@ -0,0 +1,17 @@
+//
+//  AvailabilityView.swift
+//  Osiris
+//
+//  Created by Barry on 8/10/17.
+//  Copyright © 2017 Barry. All rights reserved.
+//
+
+import UIKit
+
+class AvailabilityView: UIView {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+
+}
